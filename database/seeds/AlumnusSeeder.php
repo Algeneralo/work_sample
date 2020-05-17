3adc2226699dae05ac3fa389f03d549e8fa7c8c9
obayd <obayda.alesawi@gmail.com>
2020-05-17 08:05:38 +0300

    add static seeder data
    Former-commit-id: 09180d83093e7446dd88ae619d9552319ccefd4e

--- database/seeds/AlumnusSeeder.php
+++ database/seeds/AlumnusSeeder.php
@@ -11,8 +11,11 @@ class AlumnusSeeder extends Seeder
      */
     public function run()
     {
+        $dev = factory(\App\Models\Alumnus::class)->create(["is_team_member" => 0, "email" => "alumni@development.it"]);
         $alumni = factory(\App\Models\Alumnus::class, 3)->create(["is_team_member" => 0]);
+
         $imageUrl = Faker\Factory::create()->imageUrl(640, 480, "business", false);
+        $dev->addMediaFromUrl($imageUrl)->toMediaCollection('avatar');
         foreach ($alumni as $alumnus) {
             $alumnus->addMediaFromUrl($imageUrl)->toMediaCollection('avatar');
         }
