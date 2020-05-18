dc43e55b726110abc1be420703caad5fd404435d
obayd <obayda.alesawi@gmail.com>
2020-05-19 00:02:32 +0300

    fix event issue

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Alumnus;
+use App\Models\Event;
 use App\Models\Media;
 use Illuminate\Http\Request;
 
@@ -12,7 +13,7 @@ class HomeController extends Controller
     public function __invoke()
     {
         $alumniCount = Alumnus::query()->count();
-        $eventsCount = Alumnus::query()->count();
+        $eventsCount = Event::query()->count();
         $mediaCount = Media::query()->count();
         return view("admin.dashboard", compact("alumniCount", "eventsCount", "mediaCount"));
     }
