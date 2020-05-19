1be888ee36ad7d9c12601b61d78fdb3ae20b7d34
obayd <obayda.alesawi@gmail.com>
2020-05-20 01:17:04 +0300

    fix default data issue

--- resources/assets/js/laravel/admin/pages/offer/index.js
+++ resources/assets/js/laravel/admin/pages/offer/index.js
@@ -1,13 +1,19 @@
 require("../../../bootstrap")
 
 import ImagePreviewer from "../../../components/ImagePreviewer";
+import ImageUploader from "../../../components/ImageUploader";
+import DatePicker from 'vue2-datepicker';
 
 new Vue({
     el: ".offer",
-    components: {ImagePreviewer},
-    methods: {
-        deleteImage(id) {
-            console.log(id)
-        }
-    }
+    components: {ImagePreviewer, ImageUploader, DatePicker},
+    data: {
+        date: ''
+    },
+    mounted() {
+        document.querySelectorAll('[data-change-v-model-value]').forEach(element => {
+            let field = JSON.parse('' + element.getAttribute("data-change-v-model-value").replace(/'/g, '"'));
+            this[Object.keys(field)[0]] = Object.values(field)[0]
+        });
+    },
 })
