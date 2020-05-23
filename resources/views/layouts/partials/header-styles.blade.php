254ebc2d93565e2a4ef61c376910d77b8e4d46cd
obayd <obayda.alesawi@gmail.com>
2020-05-24 01:15:57 +0300

    change icon

--- resources/views/layouts/partials/header-styles.blade.php
+++ resources/views/layouts/partials/header-styles.blade.php
@@ -1,5 +1,5 @@
-<link rel="shortcut icon" href="{{ asset('media/favicons/favicon.png') }}">
-<link rel="icon" sizes="192x192" type="image/png" href="{{ asset('favicons/favicon.png') }}">
+<link rel="shortcut icon" href="{{ asset('media/favicons/favicon.svg') }}">
+<link rel="icon" sizes="192x192" type="image/svg" href="{{ asset('media/favicons/favicon.svg') }}">
 
 @yield('css_before')
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.css"
