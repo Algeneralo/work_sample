cfe89299b6d9ec3be30691d7fd747697da50b409
obayd <obayda.alesawi@gmail.com>
2020-05-25 02:02:19 +0300

    fix media count

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Alumnus;
 use App\Models\Event;
+use App\Models\Gallery;
 use App\Models\Media;
+use App\Models\Podcast;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -14,7 +16,7 @@ class HomeController extends Controller
     {
         $alumniCount = Alumnus::query()->count();
         $eventsCount = Event::query()->count();
-        $mediaCount = Media::query()->count();
+        $mediaCount = Gallery::query()->count() + Podcast::query()->count();
         return view("admin.dashboard", compact("alumniCount", "eventsCount", "mediaCount"));
     }
 }
