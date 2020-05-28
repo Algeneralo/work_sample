46ce36a7604518acde451abba3f5b28447039a7d
obayd <obayda.alesawi@gmail.com>
2020-05-28 13:50:49 +0300

    update favicon

--- resources/views/layouts/partials/header-styles.blade.php
+++ resources/views/layouts/partials/header-styles.blade.php
@@ -1,5 +1,5 @@
-<link rel="shortcut icon" href="{{ asset('media/favicons/favicon.svg') }}">
-<link rel="icon" sizes="192x192" type="image/svg" href="{{ asset('media/favicons/favicon.svg') }}">
+<link rel="shortcut icon" href="{{ asset('media/favicons/favicon.png') }}">
+<link rel="icon" sizes="192x192" type="image/svg" href="{{ asset('media/favicons/favicon.png') }}">
 
 @yield('css_before')
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.css"
