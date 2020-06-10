e267a101dd19f25ac583f50b4658c8bd8d2b09ba
obayd <obayda.alesawi@gmail.com>
2020-06-10 11:12:59 +0300

    add missing avatar to user

--- app/Http/Resources/Api/User/UserJsonResource.php
+++ app/Http/Resources/Api/User/UserJsonResource.php
@@ -16,6 +16,7 @@ class UserJsonResource extends JsonResource
     public function toArray($request)
     {
         return [
+            "avatar" => $this->avatar,
             "first_name" => $this->first_name,
             "last_name" => $this->last_name,
             "gender" => $this->gender,
