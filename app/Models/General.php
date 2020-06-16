08852b68798e81600ba87540a7896894505f0ec0
obayd <obayda.alesawi@gmail.com>
2020-06-16 15:33:40 +0300

    fix wrong search attributes

--- app/Models/General.php
+++ app/Models/General.php
@@ -38,10 +38,8 @@ class General extends Model implements HasMedia
     {
         return empty($string) ? static::query()
             : static::where(function ($query) use ($string) {
-                $query->where('name', 'like', '%' . $string . '%')
-                    ->orWhere('date', 'like', '%' . $string . '%')
-                    ->orWhere('start_time', 'like', '%' . $string . '%')
-                    ->orWhere('end_time', 'like', '%' . $string . '%');
+                $query->where('title', 'like', '%' . $string . '%')
+                    ->orWhere('date', 'like', '%' . $string . '%');
             });
 
     }
