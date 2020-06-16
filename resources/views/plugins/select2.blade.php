cf4a29639fd8f54dfe2b3b08e5193561e708bc18
obayd <obayda.alesawi@gmail.com>
2020-06-16 16:22:19 +0300

    remove search field for less than 7 results

--- resources/views/plugins/select2.blade.php
+++ resources/views/plugins/select2.blade.php
@@ -2,13 +2,18 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/i18n/de.js"></script>
 <script>
+    let minimumResultsForSearch=7
     $(document).ready(function () {
-        $(".select2").select2();
+        $(".select2").select2({
+            minimumResultsForSearch: minimumResultsForSearch,
+        });
     });
     //re initialize select2 after page livewire request
     document.addEventListener("livewire:load", function (event) {
         window.livewire.hook('afterDomUpdate', () => {
-            $(".select2").select2()
+            $(".select2").select2({
+                minimumResultsForSearch: minimumResultsForSearch,
+            })
         });
     });
 
