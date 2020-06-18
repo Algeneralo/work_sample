a5ff05daf174b0b4fe538aee5ef579479e6874aa
obayd <obayda.alesawi@gmail.com>
2020-06-18 18:39:52 +0300

    add read only props

--- resources/assets/js/laravel/components/Address.vue
+++ resources/assets/js/laravel/components/Address.vue
@@ -3,18 +3,23 @@
         <label class="col-12">{{label}}</label>
         <div class="col-9 form-group mb-0">
             <input type="text" class="form-control" :name="`${prefix}street`"
+                   v-bind:readonly="readOnly ? true : false"
                    :value="this.street" :placeholder="trans('general.street')" required>
         </div>
         <div class="col-3 form-group mb-0 pl-0">
-            <input type="text" class="form-control" :name="`${prefix}street_number`" :value="streetnumber"
+            <input type="text" class="form-control" :name="`${prefix}street_number`"
+                   v-bind:readonly="readOnly ? true : false" :value="streetnumber"
                    :placeholder="trans('general.number')" required>
         </div>
         <div class="col-4 form-group mb-0 pr-0 mt-2">
-            <input type="text" class="form-control" :name="`${prefix}postcode`" :value="postcode"
+            <input type="text" class="form-control" :name="`${prefix}postcode`"
+                   v-bind:readonly="readOnly ? true : false" :value="postcode"
                    :placeholder="trans('general.postcode')" required>
         </div>
         <div class="col-8 form-group mb-0 mt-2">
-            <input type="text" class="form-control" :name="`${prefix}city`" :value="city"
+            <input type="text" class="form-control" :name="`${prefix}city`"
+                   v-bind:readonly="readOnly ? true : false"
+                   :value="city"
                    :placeholder="trans('general.city')" required>
         </div>
     </div>
@@ -28,6 +33,7 @@
             street: {default: ''},
             streetnumber: {default: ''},
             postcode: {default: ''},
+            readOnly: {default: false},
             city: {default: ''},
             label: {
                 default: function () {
