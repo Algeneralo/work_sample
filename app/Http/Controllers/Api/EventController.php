0852c0ca5bdba236161d0207318d2c5bb73647b8
obayd <obayda.alesawi@gmail.com>
2020-06-19 15:20:37 +0300

    cleanup code, remove unused functions

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -2,36 +2,47 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\EventStoreRequest;
 use App\Http\Resources\Api\Event\EventJsonResource;
 use App\Http\Resources\Api\Event\EventResource;
 use App\Models\Event;
-use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
+use Throwable;
 
 class EventController extends ApiController
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
      */
     public function index()
     {
-        $events = new EventResource(Event::query()->paginate(10));
+        $events = new EventResource(
+            Event::query()
+                ->when(request()->has("userEvents"), function (Builder $query) {
+                    $query->whereHas("participants", function (Builder $query) {
+                        $query->where("alumnus_id", auth()->id());
+                    });
+                })
+                ->paginate(10)
+        );
         return $this->successResponse(["events" => $events]);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
+     * @param EventStoreRequest $request
+     * @return Response
+     * @throws Throwable
      */
     public function store(EventStoreRequest $request)
     {
-        return \DB::transaction(function () use ($request) {
+        return DB::transaction(function () use ($request) {
             /** @var Event $event */
             $event = Event::query()->create($request->all());
             $event->addMediaFromRequest("image")
@@ -44,37 +55,14 @@ class EventController extends ApiController
     /**
      * Display the specified resource.
      *
-     * @param \App\Models\Event $event
-     * @return \Illuminate\Http\Response
+     * @param Event $event
+     * @return JsonResponse
      */
     public function show(Event $event)
     {
-        \request()->merge(["show" => true]);
+        request()->merge(["show" => true]);
         $event = new EventJsonResource($event);
         return $this->successResponse(["event" => $event]);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param \App\Models\Event $event
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Event $event)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param \App\Models\Event $event
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Event $event)
-    {
-        //
     }
 
 }
