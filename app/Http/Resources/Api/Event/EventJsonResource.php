424679e030466e35be81713e692bcb78406f7ebd
obayda <obayda.alesawi@gmail.com>
2020-07-02 17:16:19 +0300

    edit response to fit external events

--- app/Http/Resources/Api/Event/EventJsonResource.php
+++ app/Http/Resources/Api/Event/EventJsonResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Api\Event;
 
+use App\Models\Event;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class EventJsonResource extends JsonResource
@@ -21,15 +22,21 @@ class EventJsonResource extends JsonResource
             "time" => $this->from_to_time,
             "cover" => $this->cover,
             //get only those values if it's a show method
-            $this->mergeWhen($request->has("show"), [
-                "rate" => $this->rate,
-                "details" => $this->details,
-                "is_participated" => $this->participants->contains(auth()->id()),
-                "free_places" => $this->free_places,
-                "reviews_count" => $this->reviews->count(),
-                "reviews" => EventReviewsJsonResources::collection($this->reviews),
-                "participants" => EventParticipantsJsonResources::collection($this->participants),
-            ]),
+            $this->mergeWhen($request->has("show"), function () {
+                return [
+                    "details" => $this->details,
+                ];
+            }),
+            $this->mergeWhen(($request->has("show") && $this->type == Event::INTERNAL_EVENTS), function () {
+                return [
+                    "rate" => $this->rate,
+                    "is_participated" => $this->participants->contains(auth()->id()),
+                    "free_places" => $this->free_places,
+                    "reviews_count" => $this->reviews->count(),
+                    "reviews" => EventReviewsJsonResources::collection($this->reviews),
+                    "participants" => EventParticipantsJsonResources::collection($this->participants),
+                ];
+            }),
         ];
     }
 }
